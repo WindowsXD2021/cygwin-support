5d42aba0af75e6167b60daf6b1ef826b48738cb8
Ignacy Swiatecki <ignac.swiatecki@gmail.com>
2021-04-28 10:35:04 +0200

    Check

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
+// The module "vscode" contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const { exec } = require("node:child_process");
+const { exists } = require("node:fs");
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,16 +14,20 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cygwin-support" is now active!');
+	console.log("Extension started...");
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cygwin-support.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand("cygwin-support.connect", function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Cygwin Support!');
+		vscode.window.showInformationMessage("Connecting to Cygwin...");
+
+		exec("cygwin", (stderr, stdout, error) => {
+			if (error) vscode.window.showErrorMessage("Cygwin not found. This extension require Cygwin to work.")
+		})
 	});
 
 	context.subscriptions.push(disposable);
